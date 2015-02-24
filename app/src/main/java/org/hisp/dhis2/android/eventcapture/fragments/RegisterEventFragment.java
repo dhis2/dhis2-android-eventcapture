54a702cc1b0d7d1071b15176ae68c31a35510457
simernes <simen_russnes@hotmail.com>
2015-02-24 16:48:10 +0100

    updated copyright notice

--- app/src/main/java/org/hisp/dhis2/android/eventcapture/fragments/RegisterEventFragment.java
+++ app/src/main/java/org/hisp/dhis2/android/eventcapture/fragments/RegisterEventFragment.java
@@ -1,3 +1,32 @@
+/*
+ *  Copyright (c) 2015, University of Oslo
+ *  * All rights reserved.
+ *  *
+ *  * Redistribution and use in source and binary forms, with or without
+ *  * modification, are permitted provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *  * list of conditions and the following disclaimer.
+ *  *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *  * this list of conditions and the following disclaimer in the documentation
+ *  * and/or other materials provided with the distribution.
+ *  * Neither the name of the HISP project nor the names of its contributors may
+ *  * be used to endorse or promote products derived from this software without
+ *  * specific prior written permission.
+ *  *
+ *  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ *  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 package org.hisp.dhis2.android.eventcapture.fragments;
 
 import android.app.Fragment;
@@ -119,14 +148,12 @@ public class RegisterEventFragment extends Fragment {
             OptionSet optionSet = MetaDataController.getOptionSet(dataElement.getOptionSet());
             if ( optionSet == null )
             {
-                dataElementViewAbstract = new TextDataElementView( getActivity().
-                        getApplicationContext(), programStageDataElement,
+                dataElementViewAbstract = new TextDataElementView( getActivity(), programStageDataElement,
                         dataElement, dataValue );
             }
             else
             {
-                dataElementViewAbstract = new OptionSetDataElementView( getActivity().
-                        getApplicationContext(), programStageDataElement,
+                dataElementViewAbstract = new OptionSetDataElementView( getActivity(), programStageDataElement,
                         dataElement, dataValue );
             }
         }
@@ -134,32 +161,27 @@ public class RegisterEventFragment extends Fragment {
         {
             if ( dataType.equalsIgnoreCase( DataElement.VALUE_TYPE_BOOL ) )
             {
-                dataElementViewAbstract = new BoolDataElementView( getActivity().
-                        getApplicationContext(), programStageDataElement,
+                dataElementViewAbstract = new BoolDataElementView( getActivity(), programStageDataElement,
                         dataElement, dataValue );
             }
             else if ( dataType.equalsIgnoreCase( DataElement.VALUE_TYPE_DATE ) )
             {
-                dataElementViewAbstract = new DatePickerDataElementView( getActivity().
-                        getApplicationContext(), programStageDataElement,
+                dataElementViewAbstract = new DatePickerDataElementView( getActivity(), programStageDataElement,
                         dataElement, dataValue );
             }
             else if ( dataType.equalsIgnoreCase( DataElement.VALUE_TYPE_TRUE_ONLY ) )
             {
-                dataElementViewAbstract = new TrueOnlyDataElementView( getActivity().
-                        getApplicationContext(), programStageDataElement,
+                dataElementViewAbstract = new TrueOnlyDataElementView( getActivity(), programStageDataElement,
                         dataElement, dataValue );
             }
             else if ( dataType.equalsIgnoreCase( DataElement.VALUE_TYPE_NUMBER ) || dataType.equalsIgnoreCase( DataElement.VALUE_TYPE_INT ) )
             {
-                dataElementViewAbstract = new NumberDataElementView( getActivity().
-                        getApplicationContext(), programStageDataElement,
+                dataElementViewAbstract = new NumberDataElementView( getActivity(), programStageDataElement,
                         dataElement, dataValue );
             }
             else
             {
-                dataElementViewAbstract = new TextDataElementView( getActivity().
-                        getApplicationContext(), programStageDataElement,
+                dataElementViewAbstract = new TextDataElementView( getActivity(), programStageDataElement,
                         dataElement, dataValue );
             }
         }
@@ -197,7 +219,6 @@ public class RegisterEventFragment extends Fragment {
         for(DataValue dataValue: dataValues) {
             dataValue.save(false);
         }
-        Dhis2.getInstance().getDataValueController().sendLocalData();
     }
 
     public void showSelectProgramFragment() {
--- app/src/main/java/org/hisp/dhis2/android/eventcapture/fragments/SelectProgramFragment.java
+++ app/src/main/java/org/hisp/dhis2/android/eventcapture/fragments/SelectProgramFragment.java
@@ -1,3 +1,32 @@
+/*
+ *  Copyright (c) 2015, University of Oslo
+ *  * All rights reserved.
+ *  *
+ *  * Redistribution and use in source and binary forms, with or without
+ *  * modification, are permitted provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *  * list of conditions and the following disclaimer.
+ *  *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *  * this list of conditions and the following disclaimer in the documentation
+ *  * and/or other materials provided with the distribution.
+ *  * Neither the name of the HISP project nor the names of its contributors may
+ *  * be used to endorse or promote products derived from this software without
+ *  * specific prior written permission.
+ *  *
+ *  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ *  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 package org.hisp.dhis2.android.eventcapture.fragments;
 
 import android.app.Fragment;
@@ -64,7 +93,8 @@ public class SelectProgramFragment extends Fragment {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 selectedOrganisationUnit = assignedOrganisationUnits.get(position); //displaying first as default
                 programsForSelectedOrganisationUnit = Dhis2.getInstance().getMetaDataController().
-                        getProgramsForOrganisationUnit(selectedOrganisationUnit.getId());
+                        getProgramsForOrganisationUnit(selectedOrganisationUnit.getId(),
+                                Program.SINGLE_EVENT_WITHOUT_REGISTRATION);
                 if(programsForSelectedOrganisationUnit == null ||
                         programsForSelectedOrganisationUnit.size() <= 0) {
                     populateSpinner(programSpinner, new ArrayList<String>());
@@ -97,6 +127,8 @@ public class SelectProgramFragment extends Fragment {
     }
 
     public void showRegisterEventFragment() {
+        if( selectedOrganisationUnit == null || programSpinner.getSelectedItemPosition() < 0)
+            return;
         MessageEvent event = new MessageEvent(BaseEvent.EventType.showRegisterEventFragment);
         Dhis2Application.bus.post(event);
     }
