8b118b8755f5fef16860f33d1558926b7bfe123c
simernes <simen_russnes@hotmail.com>
2015-08-07 14:54:13 +0200

    Moved and created generic SelectProgramFragment in SDK

--- app/src/main/java/org/hisp/dhis/android/eventcapture/fragments/SelectProgramFragment.java
+++ app/src/main/java/org/hisp/dhis/android/eventcapture/fragments/SelectProgramFragment.java
@@ -1,92 +1,36 @@
 package org.hisp.dhis.android.eventcapture.fragments;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.util.Pair;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.raizlabs.android.dbflow.structure.Model;
 import com.squareup.otto.Subscribe;
 
-import org.hisp.dhis.android.eventcapture.EventCaptureApplication;
 import org.hisp.dhis.android.eventcapture.R;
 import org.hisp.dhis.android.eventcapture.views.FloatingActionButton;
-import org.hisp.dhis.android.sdk.activities.INavigationHandler;
 import org.hisp.dhis.android.sdk.controllers.Dhis2;
-import org.hisp.dhis.android.sdk.controllers.ResponseHolder;
-import org.hisp.dhis.android.sdk.controllers.datavalues.DataValueController;
 import org.hisp.dhis.android.sdk.controllers.metadata.MetaDataController;
 import org.hisp.dhis.android.sdk.events.OnTrackerItemClick;
-import org.hisp.dhis.android.sdk.fragments.SettingsFragment;
 import org.hisp.dhis.android.sdk.fragments.dataentry.DataEntryFragment;
-import org.hisp.dhis.android.sdk.network.http.ApiRequestCallback;
 import org.hisp.dhis.android.sdk.persistence.loaders.DbLoader;
 import org.hisp.dhis.android.sdk.persistence.models.Event;
 import org.hisp.dhis.android.sdk.persistence.models.FailedItem;
 import org.hisp.dhis.android.sdk.persistence.models.Program;
+import org.hisp.dhis.android.sdk.utils.ui.adapters.AbsAdapter;
 import org.hisp.dhis.android.sdk.utils.ui.adapters.EventAdapter;
 import org.hisp.dhis.android.sdk.utils.ui.adapters.rows.events.EventRow;
-import org.hisp.dhis.android.sdk.utils.ui.dialogs.AutoCompleteDialogFragment;
-import org.hisp.dhis.android.sdk.utils.ui.dialogs.OrgUnitDialogFragment;
 import org.hisp.dhis.android.sdk.utils.ui.dialogs.ProgramDialogFragment;
 import org.hisp.dhis.android.sdk.utils.ui.views.CardTextViewButton;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SelectProgramFragment extends Fragment
-        implements View.OnClickListener, AutoCompleteDialogFragment.OnOptionSelectedListener,
-        SwipeRefreshLayout.OnRefreshListener, LoaderManager.LoaderCallbacks<List<EventRow>> {
+public class SelectProgramFragment extends org.hisp.dhis.android.sdk.fragments.selectprogram.SelectProgramFragment {
     public static final String TAG = SelectProgramFragment.class.getSimpleName();
-    private static final String STATE = "state:SelectProgramFragment";
-    private static final int LOADER_ID = 1;
 
-    private SwipeRefreshLayout mSwipeRefreshLayout;
-    private ListView mListView;
-    private ProgressBar mProgressBar;
-    private EventAdapter mAdapter;
-
-    private CardTextViewButton mOrgUnitButton;
-    private CardTextViewButton mProgramButton;
     private FloatingActionButton mRegisterEventButton;
-
-    private SelectProgramFragmentState mState;
-    private SelectProgramFragmentPreferences mPrefs;
-
-    private INavigationHandler mNavigationHandler;
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-
-        if (activity instanceof INavigationHandler) {
-            mNavigationHandler = (INavigationHandler) activity;
-        } else {
-            throw new IllegalArgumentException("Activity must " +
-                    "implement INavigationHandler interface");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        // we need to nullify reference
-        // to parent activity in order not to leak it
-        mNavigationHandler = null;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -94,117 +38,34 @@ public class SelectProgramFragment extends Fragment
         setHasOptionsMenu(true);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_select_program, container, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        mPrefs = new SelectProgramFragmentPreferences(
-                getActivity().getApplicationContext());
-
-        mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_to_refresh_layout);
-        mSwipeRefreshLayout.setColorSchemeResources(R.color.green, R.color.blue, R.color.orange);
-        mSwipeRefreshLayout.setOnRefreshListener(this);
-
-        mListView = (ListView) view.findViewById(R.id.event_listview);
-        mAdapter = new EventAdapter(getLayoutInflater(savedInstanceState));
+    protected View getListViewHeader(Bundle savedInstanceState) {
         View header = getLayoutInflater(savedInstanceState).inflate(
                 R.layout.fragment_select_program_header, mListView, false
         );
-        mProgressBar = (ProgressBar) header.findViewById(R.id.progress_bar);
-        mProgressBar.setVisibility(View.GONE);
-
-        mListView.addHeaderView(header, TAG, false);
-        mListView.setAdapter(mAdapter);
-
-        mOrgUnitButton = (CardTextViewButton) header.findViewById(R.id.select_organisation_unit);
-        mProgramButton = (CardTextViewButton) header.findViewById(R.id.select_program);
         mRegisterEventButton = (FloatingActionButton) header.findViewById(R.id.register_new_event);
-
-        mOrgUnitButton.setOnClickListener(this);
-        mProgramButton.setOnClickListener(this);
         mRegisterEventButton.setOnClickListener(this);
-
-        mOrgUnitButton.setEnabled(true);
-        mProgramButton.setEnabled(false);
         mRegisterEventButton.hide();
+        return header;
+    }
 
-        setRefreshing(Dhis2.getInstance().isLoading());
-
-        if (savedInstanceState != null &&
-                savedInstanceState.getParcelable(STATE) != null) {
-            mState = savedInstanceState.getParcelable(STATE);
-        }
-
-        if (mState == null) {
-            // restoring last selection of program
-            Pair<String, String> orgUnit = mPrefs.getOrgUnit();
-            Pair<String, String> program = mPrefs.getProgram();
-            mState = new SelectProgramFragmentState();
-            if (orgUnit != null) {
-                mState.setOrgUnit(orgUnit.first, orgUnit.second);
-                if (program != null) {
-                    mState.setProgram(program.first, program.second);
-                }
+    @Override
+    protected View.OnClickListener getProgramButtonOnClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProgramDialogFragment fragment = ProgramDialogFragment
+                        .newInstance(SelectProgramFragment.this, mState.getOrgUnitId(),
+                                Program.ProgramType.SINGLE_EVENT_WITHOUT_REGISTRATION,
+                                Program.ProgramType.WITHOUT_REGISTRATION);
+                fragment.show(getChildFragmentManager());
             }
-        }
-
-        onRestoreState(true);
+        };
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        EventCaptureApplication.getEventBus().unregister(this);
+    protected AbsAdapter getAdapter(Bundle savedInstanceState) {
+        return new EventAdapter(getLayoutInflater(savedInstanceState));
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        EventCaptureApplication.getEventBus().register(this);
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.menu_select_program, menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            mNavigationHandler.switchFragment(
-                    new SettingsFragment(), SettingsFragment.TAG, true);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle out) {
-        out.putParcelable(STATE, mState);
-        super.onSaveInstanceState(out);
-    }
-
-    @Override
-    public void onOptionSelected(int dialogId, int position, String id, String name) {
-        switch (dialogId) {
-            case OrgUnitDialogFragment.ID: {
-                onUnitSelected(id, name);
-                break;
-            }
-            case ProgramDialogFragment.ID: {
-                onProgramSelected(id, name);
-                break;
-            }
-        }
-    }
-
-    @Override
     public Loader<List<EventRow>> onCreateLoader(int id, Bundle args) {
         if (LOADER_ID == id && isAdded()) {
             List<Class<? extends Model>> modelsToTrack = new ArrayList<>();
@@ -215,20 +76,6 @@ public class SelectProgramFragment extends Fragment
                     new SelectProgramFragmentQuery(mState.getOrgUnitId(), mState.getProgramId()));
         }
         return null;
-    }
-
-    @Override
-    public void onLoadFinished(Loader<List<EventRow>> loader, List<EventRow> data) {
-        if (LOADER_ID == loader.getId()) {
-            mProgressBar.setVisibility(View.GONE);
-            mAdapter.swapData(data);
-            setRefreshing(false);
-        }
-    }
-
-    @Override
-    public void onLoaderReset(Loader<List<EventRow>> loader) {
-        mAdapter.swapData(null);
     }
 
     @Subscribe
@@ -246,136 +93,9 @@ public class SelectProgramFragment extends Fragment
         }
     }
 
-    public void onRefreshFinished() {
-        setRefreshing(false);
-    }
-
-    @Override
-    public void onRefresh() {
-        if (isAdded()) {
-            Context context = getActivity().getBaseContext();
-            Toast.makeText(context, getString(R.string.syncing), Toast.LENGTH_SHORT).show();
-            ApiRequestCallback callback = new ApiRequestCallback() {
-                @Override
-                public void onSuccess(ResponseHolder holder) {
-                    onRefreshFinished();
-                }
-
-                @Override
-                public void onFailure(ResponseHolder holder) {
-                    onRefreshFinished();
-                }
-            };
-            Dhis2.synchronize(context, callback);
-        }
-    }
-
-    private void setRefreshing(final boolean refreshing) {
-        /* workaround for bug in android support v4 library */
-        if (mSwipeRefreshLayout.isRefreshing() != refreshing) {
-            System.out.println("VIEW: " + mSwipeRefreshLayout.isRefreshing() +
-                    " BOOL: " + refreshing);
-            mSwipeRefreshLayout.post(new Runnable() {
-                @Override
-                public void run() {
-                    mSwipeRefreshLayout.setRefreshing(refreshing);
-                }
-            });
-        }
-    }
-
-    public void onRestoreState(boolean hasUnits) {
-        mOrgUnitButton.setEnabled(hasUnits);
-        if (!hasUnits) {
-            return;
-        }
-
-        SelectProgramFragmentState backedUpState = new SelectProgramFragmentState(mState);
-        if (!backedUpState.isOrgUnitEmpty()) {
-            onUnitSelected(
-                    backedUpState.getOrgUnitId(),
-                    backedUpState.getOrgUnitLabel()
-            );
-
-            if (!backedUpState.isProgramEmpty()) {
-                onProgramSelected(
-                        backedUpState.getProgramId(),
-                        backedUpState.getProgramName()
-                );
-            }
-        }
-    }
-
-    public void onUnitSelected(String orgUnitId, String orgUnitLabel) {
-        mOrgUnitButton.setText(orgUnitLabel);
-        mProgramButton.setEnabled(true);
-
-        mState.setOrgUnit(orgUnitId, orgUnitLabel);
-        mState.resetProgram();
-
-        mPrefs.putOrgUnit(new Pair<>(orgUnitId, orgUnitLabel));
-        mPrefs.putProgram(null);
-
-        handleViews(0);
-    }
-
-    public void onProgramSelected(String programId, String programName) {
-        mProgramButton.setText(programName);
-
-        mState.setProgram(programId, programName);
-        mPrefs.putProgram(new Pair<>(programId, programName));
-        handleViews(1);
-
-        mProgressBar.setVisibility(View.VISIBLE);
-        // this call will trigger onCreateLoader method
-        getLoaderManager().restartLoader(LOADER_ID, getArguments(), this);
-    }
-
-    private String getErrorDescription(Event event) {
-        FailedItem failedItem = DataValueController.getFailedItem(FailedItem.EVENT, event.getLocalId());
-
-        if (failedItem != null) {
-            if (failedItem.getHttpStatusCode() == 200) {
-                if(failedItem.getImportSummary()!=null)
-                    return failedItem.getImportSummary().getDescription();
-            }
-            if (failedItem.getHttpStatusCode() == 401) {
-                return getString(R.string.error_401_description);
-            }
-
-            if (failedItem.getHttpStatusCode()== 408) {
-                return getString(R.string.error_408_description);
-            }
-
-            if (failedItem.getHttpStatusCode() >= 400 && failedItem.getHttpStatusCode()< 500) {
-                return getString(R.string.error_series_400_description);
-            }
-
-            if (failedItem.getHttpStatusCode()>= 500) {
-                return failedItem.getErrorMessage();
-            }
-        }
-
-        return getString(R.string.unknown_error);
-    }
-
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.select_organisation_unit: {
-                OrgUnitDialogFragment fragment = OrgUnitDialogFragment
-                        .newInstance(this);
-                fragment.show(getChildFragmentManager());
-                break;
-            }
-            case R.id.select_program: {
-                ProgramDialogFragment fragment = ProgramDialogFragment
-                        .newInstance(this, mState.getOrgUnitId(),
-                                Program.ProgramType.SINGLE_EVENT_WITHOUT_REGISTRATION,
-                                Program.ProgramType.WITHOUT_REGISTRATION);
-                fragment.show(getChildFragmentManager());
-                break;
-            }
             case R.id.register_new_event: {
                 DataEntryFragment fragment2 = DataEntryFragment.newInstance(
                         mState.getOrgUnitId(), mState.getProgramId(),
@@ -389,7 +109,7 @@ public class SelectProgramFragment extends Fragment
         }
     }
 
-    private void handleViews(int level) {
+    protected void handleViews(int level) {
         mAdapter.swapData(null);
         switch (level) {
             case 0:
