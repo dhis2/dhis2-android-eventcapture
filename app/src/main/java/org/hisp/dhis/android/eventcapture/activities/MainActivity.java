ca13f3278a5b3ec4fdc0decf4cfa73758e14ffd4
erlingfjelstad <erlingfjelstad@gmail.com>
2016-01-04 11:29:43 +0100

    MainActivity implements INavigationHandler

--- app/src/main/java/org/hisp/dhis/android/eventcapture/activities/MainActivity.java
+++ app/src/main/java/org/hisp/dhis/android/eventcapture/activities/MainActivity.java
@@ -1,8 +1,68 @@
 package org.hisp.dhis.android.eventcapture.activities;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends Activity {
+import org.hisp.dhis.android.eventcapture.R;
+import org.hisp.dhis.client.sdk.ui.activities.INavigationHandler;
+
+public class MainActivity extends AppCompatActivity implements INavigationHandler {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        showSelectorFragment();
+    }
+
+    private void showSelectorFragment() {
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void switchFragment(Fragment fragment, String tag, boolean addToBackStack) {
+        if (fragment != null) {
+            FragmentTransaction transaction =
+                    getSupportFragmentManager().beginTransaction();
+
+            transaction
+                    .setCustomAnimations(R.anim.open_enter, R.anim.open_exit)
+                    .replace(R.id.fragment_container, fragment);
+            if (addToBackStack) {
+                transaction = transaction
+                        .addToBackStack(tag);
+            }
+
+            transaction.commitAllowingStateLoss();
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 1) {
+            super.onBackPressed();
+        } else {
+            finish();
+        }
+    }
 }
