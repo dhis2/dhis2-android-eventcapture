2d647959cfa0ed858b8767aae957d19a4d834029
Araz Abishov <araz.abishov.gsoc@gmail.com>
2016-02-01 14:53:07 +0100

    Adding wrapper fragment which manages fragments from SDK.

--- app/src/main/java/org/hisp/dhis/android/eventcapture/fragments/WrapperFragment.java
+++ app/src/main/java/org/hisp/dhis/android/eventcapture/fragments/WrapperFragment.java
@@ -93,7 +93,7 @@ public class WrapperFragment extends Fragment implements View.OnClickListener {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_container, container, false);
+        return inflater.inflate(R.layout.fragment_wrapper, container, false);
     }
 
     @Override
