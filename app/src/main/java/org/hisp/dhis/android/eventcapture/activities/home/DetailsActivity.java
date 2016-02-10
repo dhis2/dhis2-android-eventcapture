1f3a206b3e9504fa842826a396e78086b99bcb78
Araz Abishov <araz.abishov.gsoc@gmail.com>
2016-02-10 16:48:33 +0100

    Fixed import issues.

--- app/src/main/java/org/hisp/dhis/android/eventcapture/activities/home/DetailsActivity.java
+++ app/src/main/java/org/hisp/dhis/android/eventcapture/activities/home/DetailsActivity.java
@@ -3,14 +3,18 @@ package org.hisp.dhis.android.eventcapture.activities.home;
 
 import android.os.Bundle;
 
-import org.hisp.dhis.android.eventcapture.fragments.WrapperFragment;
+import org.hisp.dhis.android.eventcapture.R;
 import org.hisp.dhis.android.eventcapture.fragments.itemlist.ItemListFragment;
+import org.hisp.dhis.client.sdk.ui.fragments.WrapperFragment;
+
 
 public class DetailsActivity extends HomeActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        attachFragment(WrapperFragment.newInstanceWithItemlistFragment(this));
+
+        attachFragment(WrapperFragment.newInstance(ItemListFragment.class,
+                getString(R.string.drawer_item_events)));
     }
 }
