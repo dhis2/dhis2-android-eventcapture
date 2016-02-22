0d62f635ab506c0c8f39255bb323c4ff676efc3c
erlingfjelstad <erlingfjelstad@gmail.com>
2016-02-22 13:16:31 +0100

    Added animations on section change in dataentry activity

--- app/src/main/java/org/hisp/dhis/android/eventcapture/fragments/dataentry/DataEntryActivity.java
+++ app/src/main/java/org/hisp/dhis/android/eventcapture/fragments/dataentry/DataEntryActivity.java
@@ -3,15 +3,16 @@ package org.hisp.dhis.android.eventcapture.fragments.dataentry;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.PagerTitleStrip;
 import android.support.v4.view.ViewPager;
-import android.util.TypedValue;
+import android.support.v4.view.ViewPager.SimpleOnPageChangeListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextSwitcher;
 
 import org.hisp.dhis.android.eventcapture.R;
 import org.hisp.dhis.android.eventcapture.fragments.profile.ProfileFragment;
@@ -21,21 +22,27 @@ public class DataEntryActivity extends FragmentActivity {
     private ViewPager viewPager;
     private PagerAdapter pagerAdapter;
     private AppBarLayout appBarLayout;
-    private TabLayout tabLayout;
-    private PagerTitleStrip pagerTitleStrip;
+    private TextSwitcher backButtonTextSwitcher,
+                         forwardButtonTextSwitcher,
+                         sectionLabelTextSwitcher;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eventdataentry);
         viewPager = (ViewPager) findViewById(R.id.viewpager_eventdataentry_fragment);
         appBarLayout = (AppBarLayout) findViewById(R.id.appBarLayout);
-        pagerTitleStrip = (PagerTitleStrip) findViewById(R.id.pagerTitleStrip);
-        pagerTitleStrip.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
-//        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+//        backButtonTextSwitcher = (TextSwitcher) findViewById(R.id.back_button_textswitcher_eventdataentry);
+//        forwardButtonTextSwitcher = (TextSwitcher) findViewById(R.id.forward_button_textswitcher_eventdataentry);
+        sectionLabelTextSwitcher = (TextSwitcher) findViewById(R.id.textswitcher_eventdataentry);
 
+
+        viewPager.addOnPageChangeListener(new DataEntrySectionPageChangedListener(
+                        backButtonTextSwitcher,
+                        forwardButtonTextSwitcher,
+                        sectionLabelTextSwitcher));
 
         viewPager.setAdapter(new DataEntrySectionPageAdapter(getSupportFragmentManager()));
-//        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
@@ -64,5 +71,80 @@ public class DataEntryActivity extends FragmentActivity {
         public int getCount() {
             return 7;
         }
+    }
+
+    private class DataEntrySectionPageChangedListener extends SimpleOnPageChangeListener {
+        private TextSwitcher backButtonTextSwitcher,
+                forwardButtonTextSwitcher,
+                sectionLabelTextSwitcher;
+
+        private int lastPosition;
+        private final static int MAX_PAGES = 7;
+        private Animation slideOut, slideIn;
+
+        public DataEntrySectionPageChangedListener(TextSwitcher backButtonTextSwitcher,
+                                                   TextSwitcher forwardButtonTextSwitcher,
+                                                   TextSwitcher sectionLabelTextSwitcher) {
+            super();
+            this.backButtonTextSwitcher = backButtonTextSwitcher;
+            this.forwardButtonTextSwitcher = forwardButtonTextSwitcher;
+            this.sectionLabelTextSwitcher = sectionLabelTextSwitcher;
+            this.slideOut = AnimationUtils.
+                    loadAnimation(
+                            sectionLabelTextSwitcher.getContext(), android.R.anim.slide_out_right);
+            this.slideIn = AnimationUtils
+                    .loadAnimation(
+                            sectionLabelTextSwitcher.getContext(), android.R.anim.slide_in_left);
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            super.onPageSelected(position);
+            animateUiChanges(position);
+        }
+
+        private void animateUiChanges(int position) {
+            sectionLabelTextSwitcher.setText("Section " + position);
+
+//            switch (position) {
+//                case 0: {
+//                    backButtonTextSwitcher.setText("");
+//                    forwardButtonTextSwitcher.setText(">");
+//                }
+//                case (MAX_PAGES): {
+//                    backButtonTextSwitcher.setText("<");
+//                    forwardButtonTextSwitcher.setText("");
+//                }
+//                default: {
+//                    backButtonTextSwitcher.setText("<");
+//                    forwardButtonTextSwitcher.setText(">");
+//                }
+//            }
+
+
+            if(position >= lastPosition) {
+                sectionLabelTextSwitcher.setInAnimation(slideOut);
+                sectionLabelTextSwitcher.setOutAnimation(slideIn);
+
+//                backButtonTextSwitcher.setInAnimation(slideOut);
+//                backButtonTextSwitcher.setOutAnimation(slideIn);
+//
+//                forwardButtonTextSwitcher.setInAnimation(slideOut);
+//                forwardButtonTextSwitcher.setOutAnimation(slideIn);
+            }
+            else {
+                sectionLabelTextSwitcher.setInAnimation(slideIn);
+                sectionLabelTextSwitcher.setOutAnimation(slideOut);
+
+//                backButtonTextSwitcher.setInAnimation(slideIn);
+//                backButtonTextSwitcher.setOutAnimation(slideOut);
+//
+//                forwardButtonTextSwitcher.setInAnimation(slideIn);
+//                forwardButtonTextSwitcher.setOutAnimation(slideOut);
+            }
+
+            lastPosition = position;
+        }
+
     }
 }
