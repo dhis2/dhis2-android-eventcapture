4fb772ed9a070230f0dc7a651a0a5d3e5df79410
vladisla <vladisla@student.matnat.uio.no>
2016-03-22 10:43:03 +0100

    Swipe to refresh implementation problem.
    The problem is that the spinner isn't shown when events is first started.
    But it works fine afterwards.

--- app/src/main/java/org/hisp/dhis/android/eventcapture/fragments/selector/SelectorFragment.java
+++ app/src/main/java/org/hisp/dhis/android/eventcapture/fragments/selector/SelectorFragment.java
@@ -2,25 +2,20 @@ package org.hisp.dhis.android.eventcapture.fragments.selector;
 
 
 import android.app.Activity;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 
 import org.hisp.dhis.android.eventcapture.EventCaptureApp;
 import org.hisp.dhis.android.eventcapture.R;
-import org.hisp.dhis.android.eventcapture.activities.home.DetailsActivity;
 import org.hisp.dhis.android.eventcapture.fragments.itemlist.ItemListFragment;
 import org.hisp.dhis.android.eventcapture.fragments.picker.OrganisationUnitProgramPickerFragment;
 import org.hisp.dhis.android.eventcapture.utils.RxBus;
@@ -28,21 +23,16 @@ import org.hisp.dhis.client.sdk.models.organisationunit.OrganisationUnit;
 import org.hisp.dhis.client.sdk.models.program.Program;
 import org.hisp.dhis.client.sdk.ui.fragments.BaseFragment2;
 
-import fr.castorflex.android.circularprogressbar.CircularProgressBar;
 import rx.Observable;
+import timber.log.Timber;
 
-public class SelectorFragment extends BaseFragment2 implements ISelectorView,
-        OnAllPickersSelectedListener, View.OnClickListener {
+public class SelectorFragment extends BaseFragment2 implements ISelectorView, SwipeRefreshLayout.OnRefreshListener {
 
     public static final String TAG = SelectorFragment.class.getSimpleName();
-    public static final String FLOATING_BUTTON_STATE = "state:FloatingButtonState";
 
     private RxBus rxBus;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
     private ISelectorPresenter mSelectorPresenter;
-    private CircularProgressBar progressBar;
-    private FloatingActionButton mFloatingActionButton;
-    private boolean hiddenFloatingActionButton;
-    //to save the state of the action button.
 
     private OrganisationUnitProgramPickerFragment mOrganisationUnitProgramPickerFragment;
     private ItemListFragment mItemListFragment;
@@ -50,21 +40,9 @@ public class SelectorFragment extends BaseFragment2 implements ISelectorView,
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (savedInstanceState != null) {
-            hiddenFloatingActionButton = savedInstanceState.getBoolean(FLOATING_BUTTON_STATE,
-                    hiddenFloatingActionButton);
-        }
-
         mSelectorPresenter = new SelectorPresenter(this);
         mSelectorPresenter.onCreate();
-        showRefreshButton();
         setOnMenuItemClickListener(this);
-
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
     }
 
     @Override
@@ -75,16 +53,13 @@ public class SelectorFragment extends BaseFragment2 implements ISelectorView,
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_selector, container, false);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         if (savedInstanceState == null) {
-
             //init orgPicker:
             OrganisationUnitProgramPickerFragment organisationUnitProgramPickerFragment =
                     (OrganisationUnitProgramPickerFragment) createPickerFragment();
@@ -96,38 +71,23 @@ public class SelectorFragment extends BaseFragment2 implements ISelectorView,
                 attachFragment(R.id.item_fragment, mItemListFragment,
                         ItemListFragment.TAG);
             }
-            hiddenFloatingActionButton = true;
-        } else {
-            hiddenFloatingActionButton = savedInstanceState.getBoolean(FLOATING_BUTTON_STATE,
-                    hiddenFloatingActionButton);
         }
+        mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh_selector);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
 
-        mFloatingActionButton = (FloatingActionButton) view.findViewById(R.id
-                .floating_action_button);
-        mFloatingActionButton.setOnClickListener(this);
-
-        boolean onTablet = getResources().getBoolean(org.hisp.dhis.android.eventcapture.R.bool
-                .isTablet);
-        if (!onTablet) {
-            if (hiddenFloatingActionButton) {
-                mFloatingActionButton.hide();
-            } else {
-                mFloatingActionButton.show();
-            }
-        } else { //hide it
-            mFloatingActionButton.hide();
-        }
-
-        progressBar = (CircularProgressBar) view.findViewById(R.id.progress_bar_circular);
-        //hideProgress();
-
-
+        showRefreshButton();
         mSelectorPresenter.initializeSynchronization(false);
+    }
 
+    @Override
+    public void onRefresh() {
+        System.out.println("On refresh selector fragment.");
+        mSelectorPresenter.initializeSynchronization(true);
     }
 
     @Override
     public boolean onMenuItemClick(MenuItem item) {
+        System.out.println("On refresh selector fragment.");
         mSelectorPresenter.initializeSynchronization(true);
         return true;
     }
@@ -137,11 +97,6 @@ public class SelectorFragment extends BaseFragment2 implements ISelectorView,
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
     public void attachFragment(int resId, Fragment fragment, String tag) {
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction().add(resId, fragment, tag).commit();
@@ -149,18 +104,46 @@ public class SelectorFragment extends BaseFragment2 implements ISelectorView,
 
     @Override
     public void onFinishLoading() {
-        hideProgress();
+        if (mSwipeRefreshLayout == null && getView() != null) {
+            mSwipeRefreshLayout = (SwipeRefreshLayout) getView().findViewById(R.id.swipe_refresh_selector);
+            if (mSwipeRefreshLayout == null) {
+                Timber.e("mSwipeRefreshLayout is NULL");
+                return;
+            }
+        }
+        mSwipeRefreshLayout.setRefreshing(false);
+
+        //A quick workaround for a null pointer exception that happens after screen rotation.
+        //TODO: Vlad : find the cause of getActivity() returning null. (RX call related.)
+        Activity a = getActivity();
+        if (a != null) {
+            FrameLayout progressFrame = (FrameLayout) a.findViewById(R.id.layer_progress_bar);
+            progressFrame.setVisibility(View.GONE);
+        }
+        //show refresh button ?
     }
     
     @Override
+    public void onStartLoading() {
+        if (mSwipeRefreshLayout == null && getView() != null) {
+            mSwipeRefreshLayout = (SwipeRefreshLayout) getView().findViewById(R.id.swipe_refresh_selector);
+            if (mSwipeRefreshLayout == null) {
+                Timber.e("mSwipeRefreshLayout is NULL");
+                return;
+            }
+        }
+        mSwipeRefreshLayout.setRefreshing(true);
+        FrameLayout progressFrame = (FrameLayout) getActivity().findViewById(R.id
+                .layer_progress_bar);
+        progressFrame.setVisibility(View.VISIBLE);
+        //hide refresh button ?
+    }
+
+     @Override
     public void onLoadingError(Throwable throwable) {
         Snackbar.make(getView(), throwable.getMessage(), Snackbar.LENGTH_LONG);
     }
 
-    @Override
-    public void onStartLoading() {
-        showProgress();
-    }
 
     @Override
     public void onPickedOrganisationUnit(Observable<OrganisationUnit> organisationUnitObservable) {
@@ -172,82 +155,16 @@ public class SelectorFragment extends BaseFragment2 implements ISelectorView,
         rxBus.send(new OnProgramPickerValueUpdated(programObservable));
     }
 
-    private void hideProgress() {
-
-        //A quick workaround for a null pointer exception that happens after screen rotation.
-        //TODO: Vlad : find the cause of getActivity() returning null. (RX call related.)
-        Activity a = getActivity();
-        if (a != null) {
-            FrameLayout progressFrame = (FrameLayout) a.findViewById(R.id.layer_progress_bar);
-            progressFrame.setVisibility(View.GONE);
-        }
-
-        //spinner:
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-            Animation anim = AnimationUtils.loadAnimation(getContext(), R.anim.out_down);
-            progressBar.startAnimation(anim);
-        }
-        progressBar.setVisibility(View.GONE);
-    }
-
-    private void showProgress() {
-
-
-        FrameLayout progressFrame = (FrameLayout) getActivity().findViewById(R.id
-                .layer_progress_bar);
-        progressFrame.setVisibility(View.VISIBLE);
-
-        //spinner:
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-            Animation anim = AnimationUtils.loadAnimation(getContext(), R.anim.in_up);
-            progressBar.startAnimation(anim);
-        }
-        progressBar.setVisibility(View.VISIBLE);
-    }
-
-    @Override
-    public void onClick(View v) {
-        // on phone(portrait): go to ItemListFragment
-
-        Intent itemsList = new Intent(getActivity(), DetailsActivity.class); //switch to activity.
-        startActivity(itemsList);
-    }
-
-    @Override
-    public void activate() {
-        // only show on phones:
-        /* deactivated FAB, as it is currently not necessary.
-        if (!getResources().getBoolean(R.bool.isTablet)) {
-            mFloatingActionButton.show();
-            hiddenFloatingActionButton = false;
-        }*/
-    }
-
-    @Override
-    public void deactivate() {
-        mFloatingActionButton.hide();
-        hiddenFloatingActionButton = true;
-    }
-
     public Fragment createPickerFragment() {
         mOrganisationUnitProgramPickerFragment = new OrganisationUnitProgramPickerFragment();
-        //these callbacks are lost. (part of the problem)
-        mOrganisationUnitProgramPickerFragment.setOnPickerClickedListener(this);
         mOrganisationUnitProgramPickerFragment.setSelectorView(this);
         return mOrganisationUnitProgramPickerFragment;
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
-        savedInstanceState.putBoolean(FLOATING_BUTTON_STATE, hiddenFloatingActionButton);
     }
 
     public static class OnOrganisationUnitPickerValueUpdated {
         private final Observable<OrganisationUnit> organisationUnitObservable;
 
-        public OnOrganisationUnitPickerValueUpdated(Observable<OrganisationUnit>
-                                                            organisationUnitObservable) {
+        public OnOrganisationUnitPickerValueUpdated(Observable<OrganisationUnit> organisationUnitObservable) {
             this.organisationUnitObservable = organisationUnitObservable;
         }
 
