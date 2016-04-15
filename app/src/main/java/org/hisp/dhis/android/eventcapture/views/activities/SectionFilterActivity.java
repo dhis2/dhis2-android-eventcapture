61a6666546dc6d903643bfcf6c2f1611766c1a7e
vladisla <vladisla@student.matnat.uio.no>
2016-04-15 16:20:04 +0200

    Added searchView to ActoinBar.

--- app/src/main/java/org/hisp/dhis/android/eventcapture/views/activities/SectionFilterActivity.java
+++ app/src/main/java/org/hisp/dhis/android/eventcapture/views/activities/SectionFilterActivity.java
@@ -5,10 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -19,6 +23,8 @@ import org.hisp.dhis.client.sdk.android.api.D2;
 import org.hisp.dhis.client.sdk.models.program.ProgramStage;
 import org.hisp.dhis.client.sdk.models.program.ProgramStageSection;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 import rx.Subscription;
@@ -28,16 +34,18 @@ import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 import timber.log.Timber;
 
-public class SectionFilterActivity extends FragmentActivity {
+public class SectionFilterActivity extends FragmentActivity implements SearchView.OnQueryTextListener {
 
     private String programStageUid;
     private List<ProgramStageSection> sectionsList;
+    private HashMap<String, ProgramStageSection> sectoinMap;
 
     private Subscription listProgramStageDataElements;
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private SearchView searchView;
 
     public List<ProgramStageSection> getSectionList() {
         return sectionsList;
@@ -60,9 +68,28 @@ public class SectionFilterActivity extends FragmentActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
+        SearchView searchView = (SearchView) findViewById(R.id.section_search);
+        searchView.setOnQueryTextListener(this);
+        //searchView.setOnQueryTextFocusChangeListener(this);
+
         //initializes the data and adapter.
         initSectionList();
     }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        ////:
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) findViewById(R.id.section_search);
+        //SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+
+
+        searchView.setOnQueryTextListener(this);
+        searchView.setOnQueryTextFocusChangeListener(this);
+        return super.onCreateOptionsMenu(menu);
+    }*/
 
     public void initSectionList() {
         listProgramStageDataElements = D2.programStages().get(programStageUid)
@@ -87,6 +114,21 @@ public class SectionFilterActivity extends FragmentActivity {
                         Timber.d(throwable.toString());
                     }
                 });
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        System.out.println("onQueryTextSubmit");
+
+        //filter the recycler view list ?
+        //mAdapter.
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        //System.out.println("onQueryTextChange");
+        return false;
     }
 
     //**********************************************************************************************
@@ -116,6 +158,8 @@ public class SectionFilterActivity extends FragmentActivity {
         public int getItemCount() {
             return mSectionList.size();
         }
+
+        //******************************************************************************************
 
         public class ViewHolder extends RecyclerView.ViewHolder {
             public TextView mTextView;
