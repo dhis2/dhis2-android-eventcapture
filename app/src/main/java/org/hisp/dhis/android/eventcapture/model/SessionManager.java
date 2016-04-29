00213018f5de9f95ec855cf282bce25aa88eee1d
vladisla <vladisla@student.matnat.uio.no>
2016-04-29 14:04:26 +0200

    Re-added the SessionManager (Accidentally deleted).

---
+++ app/src/main/java/org/hisp/dhis/android/eventcapture/model/SessionManager.java
@@ -0,0 +1,26 @@
+package org.hisp.dhis.android.eventcapture.model;
+
+/**
+ * A class to store states of the fragments.
+ */
+public class SessionManager {
+    private static SessionManager instance;
+    private boolean selectorSynced = false;
+
+    public static SessionManager getInstance() {
+        if (instance == null) {
+            instance = new SessionManager();
+        }
+        return instance;
+    }
+
+    public boolean isSelectorSynced() {
+        return selectorSynced;
+    }
+
+
+    public void setSelectorSynced(Boolean state) {
+        selectorSynced = state;
+    }
+}
+
