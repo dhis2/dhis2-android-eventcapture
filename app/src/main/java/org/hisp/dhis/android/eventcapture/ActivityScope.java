7bf71445bb85bbd7912cfdea55b6d511fb3f237a
Araz Abishov <araz.abishov.gsoc@gmail.com>
2016-05-05 14:35:30 +0200

    Added new scop annotations

---
+++ app/src/main/java/org/hisp/dhis/android/eventcapture/ActivityScope.java
@@ -0,0 +1,11 @@
+package org.hisp.dhis.android.eventcapture;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ActivityScope {
+}
