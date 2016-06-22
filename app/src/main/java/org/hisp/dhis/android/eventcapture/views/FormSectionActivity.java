cb7a2291cf5b2eb9bdc449576a99cc69e97265ac
vladisla <vladisla@student.matnat.uio.no>
2016-06-22 18:31:29 +0200

    Implemented dialog to take the user to settings if gps is disabled.

--- app/src/main/java/org/hisp/dhis/android/eventcapture/views/FormSectionActivity.java
+++ app/src/main/java/org/hisp/dhis/android/eventcapture/views/FormSectionActivity.java
@@ -4,12 +4,14 @@ import android.Manifest;
 import android.app.Activity;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
@@ -22,6 +24,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.Toolbar;
@@ -93,6 +96,7 @@ public class FormSectionActivity extends AppCompatActivity implements FormSectio
     FloatingActionButton fabComplete;
 
     FilterableDialogFragment sectionDialogFragment;
+    AlertDialog alertDialog;
 
     public static void navigateToNewEvent(Activity activity, String eventUid) {
         navigateTo(activity, eventUid, true);
@@ -176,6 +180,10 @@ public class FormSectionActivity extends AppCompatActivity implements FormSectio
 
     @Override
     protected void onStop() {
+        //don't leak the dialog
+        if (alertDialog != null) {
+            alertDialog.dismiss();
+        }
         formSectionPresenter.detachView();
         super.onStop();
     }
@@ -411,10 +419,30 @@ public class FormSectionActivity extends AppCompatActivity implements FormSectio
                         setupLocationPermissions();
                     }
                 } else {
-                    Toast.makeText(FormSectionActivity.this, R.string.gps_disabled, Toast.LENGTH_SHORT).show();
+                    showGpsDialog();
                 }
             }
         });
+    }
+
+    public void showGpsDialog() {
+        alertDialog = new AlertDialog.Builder(this)
+                .setTitle(R.string.title_gps_disabled)
+                .setMessage(R.string.gps_disabled)
+                .setPositiveButton(R.string.settings_option, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton(R.string.cancel_option, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .create();
+
+        alertDialog.show();
     }
 
     private void setupViewPager() {
